5ca8f07bdedcc9184bb8ad6f95e6ebfce760f6f4
Cindy Offley <coffley@comcast.net>
2026-05-26 02:17:23 -0400

    Add joke voting view

--- jokes/views.py
+++ jokes/views.py
@@ -1,12 +1,15 @@
+import json
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import JsonResponse
 from django.urls import reverse_lazy
 
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
 
+from .models import Joke, JokeVote
 from .forms import JokeForm
 from .models import Joke
 
@@ -54,3 +57,51 @@ class JokeUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         obj = self.get_object()
         return self.request.user == obj.user
+    
+    def vote(request, slug):
+    user = request.user
+    joke = Joke.objects.get(slug=slug)
+    data = json.loads(request.body)
+
+    vote = data['vote']
+    likes = data['likes']
+    dislikes = data['dislikes']
+
+    if user.is_anonymous:
+        msg = 'Sorry, you have to be logged in to vote.'
+    else:
+        if JokeVote.objects.filter(user=user, joke=joke).exists():
+            joke_vote = JokeVote.objects.get(user=user, joke=joke)
+
+            if joke_vote.vote == vote:
+                msg = 'Right. You told us already. Geez.'
+            else:
+                joke_vote.vote = vote
+                joke_vote.save()
+
+                if vote == -1:
+                    likes -= 1
+                    dislikes += 1
+                    msg = "Don't like it after all, huh? OK. Noted."
+                else:
+                    likes += 1
+                    dislikes -= 1
+                    msg = 'Grown on you, has it? OK. Noted.'
+        else:
+            joke_vote = JokeVote(user=user, joke=joke, vote=vote)
+            joke_vote.save()
+
+            if vote == -1:
+                dislikes += 1
+                msg = "Sorry you didn't like the joke."
+            else:
+                likes += 1
+                msg = "Yeah, good one, right?"
+
+    response = {
+        'msg': msg,
+        'likes': likes,
+        'dislikes': dislikes
+    }
+
+    return JsonResponse(response)
