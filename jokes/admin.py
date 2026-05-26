e7e30fce6223f50395d71f94a136ea37524594d2
Cindy Offley <coffley@comcast.net>
2026-05-25 22:25:32 -0400

    Register joke vote admin

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Joke, Tag
+from .models import Category, Joke, JokeVote, Tag
 
 
 @admin.register(Category)
@@ -15,14 +15,26 @@ class CategoryAdmin(admin.ModelAdmin):
         return ()
 
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    model = Tag
-    list_display = ['tag', 'created', 'updated']
+@admin.register(Joke)
+class JokeAdmin(admin.ModelAdmin):
+    model = Joke
+    list_display = ['question', 'created', 'updated']
 
     def get_readonly_fields(self, request, obj=None):
         if obj:  # editing an existing object
             return ('slug', 'created', 'updated')
+
+        return ()
+
+
+@admin.register(JokeVote)
+class JokeVoteAdmin(admin.ModelAdmin):
+    model = JokeVote
+    list_display = ['joke', 'user', 'vote']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # editing an existing object
+            return ('created', 'updated')
 
         return ()
 
